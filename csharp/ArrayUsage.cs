9159b43a0cfc5d504330991f78a9a0838aec3ec4
Sanket Saurav <sanket@deepsource.io>
2024-10-25 14:29:30 -0700

    feat: add array variable and print its string representation

--- csharp/ArrayUsage.cs
+++ csharp/ArrayUsage.cs
@@ -1,8 +1,12 @@
+using System;
+
 public class ArrayUsage
 {
   public void PrintArray()
   {
     ArrayDefinition arrayDef = new ArrayDefinition();
+    var array = arrayDef.Numbers;
+    Console.WriteLine(array.ToString());
     Console.WriteLine(arrayDef.Numbers.ToString());
   }
 }
